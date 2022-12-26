ddca69e69739b4042c28ff94858378eac7d8b13a
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-12-26 20:38:06 +0700

    SiGG-FinancialTech-Phoenix-Application/Console/Service/SiGG-FinancialTech-Phoenix-Application/Console/InternalBatchJobCheckEod

---
+++ GM.Application.Console/Services/InternalBatchJobCheckEod.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using GM.Application.Console.Model;
+using GM.ClientAPI;
+using GM.CommonLibs.Common;
+using GM.Model.ExternalInterface;
+using GM.Model.Static;
+
+namespace GM.Application.Console.Services
+{
+    public class InternalBatchJobCheckEod : WebAPI
+    {
+        private DateTime systemDate;
+        private IConsoleService _service;
+        private static Console_Entity ConsoleEnt = new Console_Entity();
+        private static Mail_AdminEntity MailAdminEnt = new Mail_AdminEntity();
+
+        public InternalBatchJobCheckEod(IHttpClientFactory httpClientFactory, IConsoleService consoleService) : base(httpClientFactory)
+        {
+            _service = consoleService;
+        }
+
+        public bool Run()
+        {
+            string StrMsg = string.Empty;
+            bool SendEmail = false;
+            try
+            {
+                _service.WriteLogs("### START RUN FUNCTION [" + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") + "] : [" + _service.GetFunction() + "()] ###");
+                List<RpConfigModel> RpConfigModel = new List<RpConfigModel>();
+
+                string inputDate = _service.GetInputDate();
+                systemDate = _service.GetBusinessDateOrSystemDate(inputDate);
+
+                var ResultRpconfig = StaticAPI.RpConfig.GetRpConfig("RP_CHECKING_EOD_MAIL", string.Empty);
+                if (!ResultRpconfig.Success)
+                {
+                    throw new Exception("GetRpConfig() => [" + ResultRpconfig.RefCode + "] " + ResultRpconfig.Message);
+                }
+
+                InterfaceCheckingEodModel CheckingEodModel = new InterfaceCheckingEodModel();
+
+                //Step 2 : Set Config
+                RpConfigModel = ResultRpconfig.Data;
+                CheckingEodModel.RpConfigModel = RpConfigModel;
+                if (!SetConfigInternalBatchJobCheckEod(ref StrMsg, ref CheckingEodModel, RpConfigModel))
+                {
+                    throw new Exception("Set_ConfigGlToSFTP() => " + StrMsg);
+                }
+
+                _service.WriteLogs("Get Config Success");
+                if (ConsoleEnt.Enable == "N")
+                {
+                    _service.WriteLogs(_service.GetFunction() + " Disable");
+                    return true;
+                }
+
+                var ResultInternalJob = ExternalInterfaceAPI.InterfaceCheckingEod.CheckingEodList(CheckingEodModel);
+                if (!ResultInternalJob.Success)
+                {
+                    throw new Exception("CheckingEodList() => [" + ResultInternalJob.RefCode + "] " + ResultInternalJob.Message);
+                }
+
+                _service.WriteLogs("InternalBatchJobCheckEod Success.");
+            }
+            catch (Exception Ex)
+            {
+                _service.WriteLogs("Error " + Ex.Message);
+                SendEmail = true;
+            }
+            finally
+            {
+                _service.WriteLogs("### END RUN FUNCTION [" + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") + "] : [" + _service.GetFunction() + "()] ###");
+                if (SendEmail)
+                {
+                    _service.SendMailError(MailAdminEnt);
+                }
+            }
+            return true;
+        }
+
+        private bool SetConfigInternalBatchJobCheckEod(ref string ReturnMsg, ref InterfaceCheckingEodModel CheckingEodModel, List<RpConfigModel> List_RpConfigModel)
+        {
+            try
+            {
+                _service.WriteLogs("");
+                ConsoleEnt.Enable = List_RpConfigModel.FirstOrDefault(a => a.item_code == "ENABLE")?.item_value;
+                _service.WriteLogs("ENABLE = " + ConsoleEnt.Enable);
+
+                MailAdminEnt.Host = List_RpConfigModel.FirstOrDefault(a => a.item_code == "MAIL_SERVER")?.item_value;
+                if (!String.IsNullOrEmpty(List_RpConfigModel.FirstOrDefault(a => a.item_code == "MAIL_PORT")?.item_value))
+                {
+                    MailAdminEnt.Port = System.Convert.ToInt32(List_RpConfigModel.FirstOrDefault(a => a.item_code == "MAIL_PORT")?.item_value);
+                }
+                MailAdminEnt.From = List_RpConfigModel.FirstOrDefault(a => a.item_code == "MAIL_SENDER")?.item_value;
+                string Mail_To_Tmp = List_RpConfigModel.FirstOrDefault(a => a.item_code == "MAIL_ADMIN")?.item_value;
+                if (!String.IsNullOrEmpty(Mail_To_Tmp))
+                {
+                    string[] mail_to_array = Mail_To_Tmp.Split(',');
+                    foreach (string mail_to in mail_to_array)
+                    {
+                        MailAdminEnt.To.Add(mail_to);
+                    }
+                }
+                MailAdminEnt.Subject = List_RpConfigModel.FirstOrDefault(a => a.item_code == "MAIL_SUBJECT")?.item_value;
+                MailAdminEnt.Subject = MailAdminEnt.Subject.Replace("{1}", "Run at " + DateTime.Now.ToString("dd MMM yyyy HH:mm:ss"));
+
+                _service.WriteLogs("MAIL_SERVER  = " + MailAdminEnt.Host);
+                _service.WriteLogs("MAIL_PORT    = " + MailAdminEnt.Port);
+                _service.WriteLogs("MAIL_SENDER  = " + MailAdminEnt.From);
+                _service.WriteLogs("MAIL_ADMIN   = " + Mail_To_Tmp);
+                _service.WriteLogs("MAIL_SUBJECT = " + MailAdminEnt.Subject);
+            }
+            catch (Exception ex)
+            {
+                ReturnMsg = ex.Message;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
